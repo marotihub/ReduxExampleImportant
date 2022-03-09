5254552e862dc044cd88bb149293be1a2263aac4
maroti <maroti1612@gmail.com>
2022-03-09 11:41:28 +0530

    git commmmme

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ export default function App() {
       <button onClick={() => dispatch(decrement())}>Decrement</button>
       <button onClick={() => dispatch(reset())}>Reset</button>
       {isLogged ? <h1>Welcome Admin</h1> : <h1>welcome guest</h1>}
+      <h1>hello viki</h1>
     </div>
   );
 }
